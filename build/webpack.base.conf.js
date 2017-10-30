016461a86e15d8f777062ea5c3e516085a98e76d
linsong <757803449@qq.com>
2017-10-30 11:02:09 +0800

    引入 jQuery

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+var webpack=require('webpack');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -63,5 +64,12 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $:"jquery",
+      jQuery:"jquery",
+      "windows.jQuery":"jquery"
+    })
+  ]
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import $ from 'jquery'
 
 Vue.config.productionTip = false
 
@@ -10,4 +11,8 @@ new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
+})
+
+$(function () {
+  alert(1111)
 })
