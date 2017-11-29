32b62e302f5c2ab98e609b2a92b2c0235d981c7f
linsong <757803449@qq.com>
2017-11-29 15:14:49 +0800

    Navigation style(small)

--- src/components/nav/Navigation.vue
+++ src/components/nav/Navigation.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-xs-offset-3 col-xs-6">
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">News</a></li>
-          <li><a href="">Kids</a></li>
-          <li><a href="">Sports</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="">About</a></li>
-        </ul>
+    <header class="nav">
+      <div style="height: 50px;width: 960px; margin:0 auto">
+        <div>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Kids</a></li>
+            <li><a href="">Sports</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">About</a></li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </header>
   </div>
+
 </template>
 <script>
   export default {
@@ -40,5 +43,11 @@
     width:100px;
     color: #8c2830;
     font-weight:600;
+  }
+  .nav{
+    background-color: lavender;
+    height:70px;
+    line-height:70px;
+    width:100%;
   }
 </style>
