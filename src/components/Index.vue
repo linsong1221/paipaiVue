44f18885561a4b10d70e606daf1d670e3bf6f174
linsong <757803449@qq.com>
2017-11-30 17:17:25 +0800

    content (small)

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <Navigation></Navigation>
+    <content></content>
   </div>
 </template>
 <script>
   import Navigation from '../components/nav/Navigation'
+  import content from '../components/content'
   export default {
     components : {
-      Navigation
+      Navigation,
+      content
     }
   }
 </script>
